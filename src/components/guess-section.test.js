7356712f06cee4cee8cc27773844c47647ca6542
Evan Thompson <evanbrowningthompson@gmail.com>
2018-03-24 13:44:04 -0400

    wrote test for guess-section component

---
+++ src/components/guess-section.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import GuessSection from './guess-section';
+
+describe('<GuessSection />', function() {
+  it('Renders without crashing', function() {
+    shallow(<GuessSection />);
+  });
+});
